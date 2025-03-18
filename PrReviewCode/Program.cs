fd023f335035526ed59253aa055c5428eabefa72
Hitesh <54570504+Hitesh97k@users.noreply.github.com>
2025-03-18 22:30:13 +0530

    Update Program.cs

--- PrReviewCode/Program.cs
+++ PrReviewCode/Program.cs
@@ -4,7 +4,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-                throw new Exception("Test Exception");
 
         var builder = WebApplication.CreateBuilder(args);
 
