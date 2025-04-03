08cfe362a0365c79b28bae4c2cbf70fc15e7e1d2
Hitesh <54570504+Hitesh97k@users.noreply.github.com>
2025-04-03 23:21:43 +0530

    Update Program.cs

--- PrReviewCode/Program.cs
+++ PrReviewCode/Program.cs
@@ -33,6 +33,9 @@ public class Program
 
         app.MapGet("/weatherforecast", (HttpContext httpContext) =>
             {                
+                
+                throw new Exception("Test Exception");
+                
                 var forecast = Enumerable.Range(1, 5).Select(index =>
                         new WeatherForecast
                         {
